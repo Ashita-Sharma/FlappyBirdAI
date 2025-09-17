841f811e75f0f2916c0e807cd4a847e7b389d1ad
Ashita-Sharma <ashitasharma2007@gmail.com>
2025-09-17 19:28:33 +0530

    Add files via upload

--- part1.py
+++ part1.py
@@ -51,6 +51,27 @@ class Bird:
         else:  # tilt down
             if self.tilt > -90:
                 self.tilt -= self.ROT_VEL
+    def draw(self, win):
+        self.img_count += 1
+
+        # For animation of bird, loop through three images
+        if self.img_count <= self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count <= self.ANIMATION_TIME*2:
+            self.img = self.IMGS[1]
+        elif self.img_count <= self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count <= self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+        #makes sure nothing is left behind
+        #when falling no flapping
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+
 while True:
     bird.move()
 
