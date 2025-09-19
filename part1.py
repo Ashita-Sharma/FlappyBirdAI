a72b5a742cbb669c852dacb52411980e564a78e9
Ashita Sharma <ashitasharma2007@gmail.com>
2025-09-19 14:45:54 +0530

    added the class base

--- part1.py
+++ part1.py
@@ -91,6 +91,12 @@ class Pipe:
     GAP = 200
     VEL = 5
 
+    def set_height(self):
+        # randomizing height of pipes
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
     def __init__(self, x):
         self.x = x
         self.height = 0
@@ -102,11 +108,6 @@ class Pipe:
         self.passed = False
         self.set_height()
 
-        def set_height(self):
-            # randomizing height of pipes
-            self.height = random.randrange(50, 450)
-            self.top = self.height - self.PIPE_TOP.get_height()
-            self.bottom = self.height + self.GAP
 
         def move(self):
             # moving of pipes to the left
@@ -118,6 +119,48 @@ class Pipe:
             # draw bottom
             win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
 
+        def collide(self, bird, win):
+            bird_mask = bird.get_mask()
+            top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+            bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+            # to check coordinates of bird and pipes
+            top_offset = (self.x - bird.x, self.top - round(bird.y))
+
+            bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+            # if they do not overlap, returns None
+            b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+            t_point = bird_mask.overlap(top_mask, top_offset)
+            # vvv makes sure b/t point are not None
+            if b_point or t_point:
+                return True
+
+            return False
+
+
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        # moves floor
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        # draws the floor as two images that are attacked to each pother with one end
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
 
 def draw_window(win, bird):
     win.blit(BG_IMG, (0, 0))
